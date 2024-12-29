a280b379fa2492ec80d689aa6390d85f07211cb6
daewan <daewan1120@naver.com>
2024-12-29 14:18:43 +0900

    Initial commit

---
+++ src/main/java/org/ide/userSetting/UserSetting.java
@@ -0,0 +1,17 @@
+package org.ide.userSetting;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UserSetting extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
